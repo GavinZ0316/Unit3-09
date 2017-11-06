bcbc7ccbde7f2bed53d393509a6eb79498f936af
Gavin Zhou <taifeng.zhou@ocsbstudent.ca>
2017-11-06 06:22:43 -0500

    Init

---
+++ Unit3-09.py
@@ -0,0 +1,7 @@
+# Created by: Gavin Zhou
+# Created on: Oct 23 2017
+# Created for: ICS3U
+
+for number in range(65,91):
+    for letter in range(97,123):
+        print(unichr(number) + '->' + unichr(letter))
